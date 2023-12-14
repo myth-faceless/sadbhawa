ea304fec372e5ede4363f37c50be157f2f99b08f
myth-faceless <mythmanish743@gmail.com>
2023-12-14 10:48:24 +0545

    Utils part

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,32 @@
+
+//By using promise you can get request handled as: 
+
+const asyncHandler = (requestHandler) => {
+    ( req, res, next ) => {
+        Promise.resolve(requestHandler( req, res, next )).
+        catch((err) => next(err))
+    }
+
+}
+
+
+
+
+//if you are using try catch to handle the request
+
+// const asyncHandler = (fn) => {
+//     async ( req, res, next ) => {
+//         try {
+//             await ( req, res, next )
+//         } catch (error) {
+//             res.status(err.code || 500).json({
+//                 success: false,
+//                 message: err.message
+//             })
+            
+//         }
+//     }
+// }
+
+
+export { asyncHandler }
