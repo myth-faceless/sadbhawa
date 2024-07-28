4223c22466d0e09a770cc090bbb50c2bf99af716
myth-faceless <mythmanish743@gmail.com>
2024-07-28 22:13:18 +0545

    update in admin model

--- src/models/admin.model.js
+++ src/models/admin.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const adminSchema = new mongoose.Schema(
     {
@@ -38,5 +40,41 @@ const adminSchema = new mongoose.Schema(
 
     }, { timestamps: true }
 )
+
+adminSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+}) 
+
+adminSchema.methods.isPasswordCorrect = async function(password) {
+   return await bcrypt.compare(password, this.password) 
+}
+
+adminSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            userName: this.userName,
+            fullName: this.fullName
+        }, 
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+adminSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const Admin = mongoose.model("Admin", adminSchema)
