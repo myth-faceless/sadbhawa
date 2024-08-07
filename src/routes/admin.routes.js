2692685695b62a832a8b9a07ad5f35693418784c
myth-faceless <mythmanish743@gmail.com>
2024-08-07 21:57:30 +0545

    updated some routes

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,13 +1,33 @@
 import { Router } from "express";
-import { createAdmin } from "../controllers/admin.controller.js";
+import {
+  createAdmin,
+  loginAdmin,
+  logoutAdmin,
+  updateAdmin,
+} from "../controllers/admin.controller.js";
 import { upload } from "../middlewares/multter.middleware.js";
 import { validate } from "../middlewares/validate.middleware.js";
-import adminSchema from "../validators/admin.validator.js";
+import {
+  registerAdminSchema,
+  loginAdminSchema,
+  updateAdminSchema,
+} from "../validators/admin.validator.js";
+import verifyJWT from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+//protected routes
 router
   .route("/register")
-  .post(upload.single("avatar"), validate(adminSchema), createAdmin);
+  .post(upload.single("avatar"), validate(registerAdminSchema), createAdmin);
+
+//public routes
+router.route("/login").post(validate(loginAdminSchema), loginAdmin);
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutAdmin);
+router
+  .route("/:adminId")
+  .put(verifyJWT, validate(updateAdminSchema), updateAdmin);
 
 export { router as adminRoutes };
