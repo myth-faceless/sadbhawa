2df99e414f181033d3736109f2b5106fb803c100
myth-faceless <mythmanish743@gmail.com>
2024-08-07 21:57:18 +0545

    removed userName

--- src/models/admin.model.js
+++ src/models/admin.model.js
@@ -50,7 +50,6 @@ adminSchema.methods.generateAccessToken = function () {
     {
       _id: this._id,
       email: this.email,
-      userName: this.userName,
       fullName: this.fullName,
     },
     process.env.ACCESS_TOKEN_SECRET,
