8318db4950d403e5ae27a5d9421b729bc52e2f11
myth-faceless <mythmanish743@gmail.com>
2024-08-07 21:56:09 +0545

    added login, logout, update functions

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,13 +1,45 @@
 import { Admin } from "../models/admin.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiErrors.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
+import ApiResponse from "../utils/ApiResponse.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { USER_ICON } from "../constants/app.constants.js";
+import {
+  STATUS_CODES,
+  ERROR_MESSAGES,
+  SUCCESS_MESSAGES,
+} from "../constants/app.constants.js";
 
-const createAdmin = asyncHandler(async (req, res) => {
+const generateToken = async (adminId) => {
+  try {
+    const admin = await Admin.findById(adminId);
+    if (!admin) {
+      throw new ApiError(
+        STATUS_CODES.NOT_FOUND,
+        ERROR_MESSAGES.ADMIN_NOT_FOUND
+      );
+    }
+    const accessToken = admin.generateAccessToken();
+    return { accessToken };
+  } catch (error) {
+    throw new ApiError(
+      STATUS_CODES.INTERNAL_SERVER_ERROR,
+      ERROR_MESSAGES.TOKEN_GENEREATION
+    );
+  }
+};
+
+const createAdmin = asyncHandler(async (req, res, next) => {
   try {
     const { fullName, phoneNumber, email, password } = req.validateBody;
+
+    const existedAdmin = await Admin.findOne({ email });
+
+    if (existedAdmin) {
+      return res
+        .status(STATUS_CODES.DUPLICATE_ENTRY)
+        .json(ApiResponse.error(ERROR_MESSAGES.USER_EMAIL_ALREADY_EXIST));
+    }
 
     const avatarLocalPath = req.file?.path;
     let avatarUrl = null;
@@ -16,29 +48,162 @@ const createAdmin = asyncHandler(async (req, res) => {
         const response = await uploadOnCloudinary(avatarLocalPath);
         avatarUrl = response.secure_url;
       } catch (uploadError) {
-        throw new ApiError(500, "Failed to upload avatar to Cloudinary", [
-          uploadError.message,
-        ]);
+        throw new ApiError(
+          STATUS_CODES.INTERNAL_SERVER_ERROR,
+          ERROR_MESSAGES.CLOUDINARY_AVATAR_UPLOAD_FAILED,
+          [uploadError.message]
+        );
       }
     } else {
       avatarUrl = USER_ICON;
     }
 
-    const admin = new Admin({
+    const admin = await Admin.create({
       fullName,
       phoneNumber,
       email,
       password,
       avatar: avatarUrl,
     });
-    await admin.save();
 
-    const response = new ApiResponse(201, admin, "Admin created successfully");
+    const response = new ApiResponse(
+      STATUS_CODES.CREATED,
+      admin,
+      SUCCESS_MESSAGES.ADMIN_REGISTERED
+    );
     res.status(201).json(response);
   } catch (err) {
-    const error = new ApiError(500, "Failed to create admin", err.stack);
+    const error = new ApiError(
+      STATUS_CODES.INTERNAL_SERVER_ERROR,
+      ERROR_MESSAGES.ERROR_CREATING_ADMIN,
+      err.stack
+    );
     res.status(500).json(error);
   }
 });
 
-export { createAdmin };
+const loginAdmin = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.validateBody;
+
+  if (!email && !password) {
+    throw new ApiError(
+      STATUS_CODES.BAD_REQUEST,
+      ERROR_MESSAGES.REQUIRED_EMAIL_PASSWORD
+    );
+  }
+
+  const admin = await Admin.findOne({ email });
+
+  if (!admin) {
+    throw new ApiError(STATUS_CODES.NOT_FOUND, ERROR_MESSAGES.USER_NOT_FOUND);
+  }
+
+  const isPasswordValid = await admin.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(
+      STATUS_CODES.UNAUTHORIZED,
+      ERROR_MESSAGES.INCORRECT_EMAIL_PASSWORD
+    );
+  }
+
+  const accessToken = await generateToken(admin._id);
+
+  const loggedInAdmin = await Admin.findById(admin._id).select("-password");
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(STATUS_CODES.SUCCESS)
+    .cookie("accessToken", accessToken, options)
+    .json(
+      new ApiResponse(
+        STATUS_CODES.SUCCESS,
+        { admin: loggedInAdmin, accessToken },
+        SUCCESS_MESSAGES.ADMIN_LOGGED_IN
+      )
+    );
+});
+
+const logoutAdmin = asyncHandler(async (req, res) => {
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(STATUS_CODES.SUCCESS)
+    .clearCookie("accessToken", options)
+    .json(
+      new ApiResponse(STATUS_CODES.SUCCESS, SUCCESS_MESSAGES.ADMIN_LOGGED_OUT)
+    );
+});
+
+const updateAdmin = asyncHandler(async (req, res, next) => {
+  try {
+    const { fullName, phoneNumber, email } = req.validateBody;
+    const { adminId } = req.params;
+    console.log(adminId);
+
+    // Check if the admin exists
+    const admin = await Admin.findById(adminId);
+    if (!admin) {
+      return next(
+        new ApiError(STATUS_CODES.NOT_FOUND, ERROR_MESSAGES.ADMIN_NOT_FOUND)
+      );
+    }
+
+    // Check for email uniqueness if email is being updated
+    if (email && email !== admin.email) {
+      const existedAdmin = await Admin.findOne({ email });
+      if (existedAdmin) {
+        return res
+          .status(STATUS_CODES.DUPLICATE_ENTRY)
+          .json(ApiResponse.error(ERROR_MESSAGES.USER_EMAIL_ALREADY_EXIST));
+      }
+    }
+
+    // Handle avatar update if a new file is uploaded
+    const avatarLocalPath = req.file?.path;
+    let avatarUrl = admin.avatar;
+
+    if (avatarLocalPath) {
+      try {
+        const response = await uploadOnCloudinary(avatarLocalPath);
+        avatarUrl = response.secure_url;
+      } catch (uploadError) {
+        throw new ApiError(
+          STATUS_CODES.INTERNAL_SERVER_ERROR,
+          ERROR_MESSAGES.CLOUDINARY_AVATAR_UPLOAD_FAILED,
+          [uploadError.message]
+        );
+      }
+    }
+
+    // Update the admin details
+    admin.fullName = fullName || admin.fullName;
+    admin.phoneNumber = phoneNumber || admin.phoneNumber;
+    admin.email = email || admin.email;
+    admin.avatar = avatarUrl;
+
+    await admin.save();
+
+    const response = new ApiResponse(
+      STATUS_CODES.SUCCESS,
+      admin,
+      SUCCESS_MESSAGES.ADMIN_UPDATED
+    );
+    res.status(200).json(response);
+  } catch (err) {
+    const error = new ApiError(
+      STATUS_CODES.INTERNAL_SERVER_ERROR,
+      ERROR_MESSAGES.ERROR_UPDATING_ADMIN,
+      err.stack
+    );
+    res.status(500).json(error);
+  }
+});
+
+export { createAdmin, loginAdmin, logoutAdmin, updateAdmin };
