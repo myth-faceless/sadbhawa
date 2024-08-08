aacae09f51c4a0888dde7b01bfd81d7cd4a98fe4
myth-faceless <mythmanish743@gmail.com>
2024-08-08 13:26:49 +0545

    added password change validator

--- src/validators/admin.validator.js
+++ src/validators/admin.validator.js
@@ -29,3 +29,12 @@ export const updateAdminSchema = Joi.object({
     .max(15),
   email: Joi.string().pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/),
 });
+
+export const changeAdminPasswordSchema = Joi.object({
+  oldPassword: Joi.string()
+    .pattern(/^[a-zA-Z0-9]{3,30}$/)
+    .required(),
+  newPassword: Joi.string()
+    .pattern(/^[a-zA-Z0-9]{3,30}$/)
+    .required(),
+});
