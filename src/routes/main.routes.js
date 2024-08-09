957ab4bc2aca8d2a7dfc26a937cf447f674234d4
myth-faceless <mythmanish743@gmail.com>
2024-08-09 23:48:12 +0545

    added doctor route

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -3,8 +3,10 @@ const router = Router();
 
 //routes import
 import { adminRoutes } from "./admin.routes.js";
+import { doctorRoutes } from "./doctor.routes.js";
 
 //routes declaration
 router.use("/admin", adminRoutes);
+router.use("/doctors", doctorRoutes);
 
 export { router as mainRoutes };
